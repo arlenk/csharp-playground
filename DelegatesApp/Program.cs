83e8f36fb40a01578099946b76196f12b27cff59
arlenk <arlen.khodadadi@gmail.com>
2020-01-03 14:26:02 -0500

    Add project files.

---
+++ DelegatesApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DelegatesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
