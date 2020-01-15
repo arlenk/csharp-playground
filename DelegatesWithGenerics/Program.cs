bad6ad23d731219ba4c8e894bf0e58f9673adc33
arlenk <arlen.khodadadi@gmail.com>
2020-01-14 22:09:13 -0500

    make printArray a generic function

--- DelegatesWithGenerics/Program.cs
+++ DelegatesWithGenerics/Program.cs
@@ -15,16 +15,10 @@ namespace DelegatesWithGenericsApp
             Predicate<double> doublePredicate = new Predicate<double>(SimplePredicate<double>);
 
             int[] filteredIntValues = FilterArray(intValues, SimplePredicate<int>);
-            foreach (int i in filteredIntValues)
-            {
-                Console.WriteLine("value: {0}", i);
-            }
+            PrintArray(filteredIntValues);
 
             double[] filteredDoubleValues = FilterArray(doubleValues, doublePredicate);
-            foreach (double i in filteredDoubleValues)
-            {
-                Console.WriteLine("double value: {0}", i);
-            }
+            PrintArray(filteredDoubleValues);
         }
 
         static bool SimplePredicate<T>(T value)
@@ -44,5 +38,12 @@ namespace DelegatesWithGenericsApp
             }
             return result.ToArray();
         }
+
+        static void PrintArray<T>(T[] array)
+        {
+            foreach (T val in array)
+                Console.WriteLine("value: {0}", val);
+        }
+
     }
 }
