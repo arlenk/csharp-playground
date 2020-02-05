a9423f9bef5da190fa5a50a189d91c5abd9a3a95
arlenk <arlen.khodadadi@gmail.com>
2020-02-04 21:26:34 -0500

    EnumApp

---
+++ EnumApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace EnumApp
+{
+    enum WeekDay
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+    }
+
+    // we can set specific values if desired
+    enum CustomValueEnum
+    {
+        a = 7,
+        b = 3,
+        c = -1,
+    }
+    
+    // enum can have a custom (must be numeric) type
+    enum ByteEnum : byte
+    {
+        Goblin,
+        Troll,
+        Hobit,
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeekDay day = WeekDay.Tuesday;
+
+            foreach (WeekDay d in Enum.GetValues(typeof(WeekDay)))
+            {
+                Console.WriteLine("{0} is a work day? {1}", d, isWorkDay(d));
+            }
+
+            foreach (CustomValueEnum v in Enum.GetValues(typeof(CustomValueEnum)))
+            {
+                Console.WriteLine("value for {0} is {1}", v, (int)v);
+            }
+
+            Console.WriteLine("day: {0}", day);
+        }
+
+        static bool isWorkDay(WeekDay day)
+        {
+            switch (day)
+            {
+                case WeekDay.Saturday:
+                case WeekDay.Sunday:
+                    return false;
+                default:
+                    return true;
+            }
+        }
+    }
+}
